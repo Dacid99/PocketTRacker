483232d95c9053ded50398cfb42482ce490b9926
david <david@aderbauer.org>
2024-06-18 22:51:22 +0200

    fixes

--- app/src/main/java/org/sbv/pockettracker/GameStatistics.java
+++ app/src/main/java/org/sbv/pockettracker/GameStatistics.java
@@ -1,6 +1,7 @@
 package org.sbv.pockettracker;
 
 import java.util.Collections;
+import java.util.NoSuchElementException;
 
 public class GameStatistics {
     ScoreSheet scoreSheet ;
@@ -10,17 +11,29 @@ public class GameStatistics {
     }
 
     public int maxRunPlayer1(){
-        return Collections.max(scoreSheet.getPlayer1IncrementsList());
+        int maxRun;
+        try {
+            maxRun = Collections.max(scoreSheet.getPlayer1IncrementsList());
+        } catch (NoSuchElementException e){ //indicating an empty list
+            maxRun = 0;
+        }
+        return maxRun;
     }
     public int maxRunPlayer2(){
-        return Collections.max(scoreSheet.getPlayer2IncrementsList());
+        int maxRun;
+        try {
+            maxRun = Collections.max(scoreSheet.getPlayer2IncrementsList());
+        } catch (NoSuchElementException e){
+            maxRun = 0;
+        }
+        return maxRun;
     }
 
     public int player1Innings(){
-        return (int) Math.ceil(scoreSheet.length()/2.0);
+        return (int) Math.ceil(Math.abs(scoreSheet.length()/2.0 - 0.5));
     }
 
     public int player2Innings(){
-        return (int) Math.floor(scoreSheet.length()/2.0);
+        return (int) Math.floor(Math.abs(scoreSheet.length()/2.0 - 0.5));
     }
 }
--- app/src/main/java/org/sbv/pockettracker/ScoreSheetActivity.java
+++ app/src/main/java/org/sbv/pockettracker/ScoreSheetActivity.java
@@ -53,15 +53,16 @@ public class ScoreSheetActivity extends AppCompatActivity {
         newTableRow.setLayoutParams(rowLayoutParams);
 
         TextView turnText = new TextView(this);
+        TextView switchReasonText = new TextView(this);
         TextView player1IncrementText = new TextView(this);
         TextView player1TotalText = new TextView(this);
         TextView player2IncrementText = new TextView(this);
         TextView player2TotalText = new TextView(this);
         TextView ballsOnTableText = new TextView(this);
-        TextView switchReasonText = new TextView(this);
 
 
         turnText.setText(getString(R.string.turnnumber_format, turn));
+        switchReasonText.setText(getString(R.string.switchReason_format, scoreSheet.getSwitchReasonAt(turn)));
         //only show increments for turnplayers
         //also not for 0th turn
         Drawable background = ContextCompat.getDrawable(this, R.drawable.cell_separator);
@@ -74,47 +75,47 @@ public class ScoreSheetActivity extends AppCompatActivity {
         player1TotalText.setText(getString(R.string.player_score_format, scoreSheet.getScoreOfPlayer1At(turn)));
         player2TotalText.setText(getString(R.string.player_score_format, scoreSheet.getScoreOfPlayer2At(turn)));
         ballsOnTableText.setText(getString(R.string.remainingBalls_format, scoreSheet.getBallsOnTableAt(turn)));
-        switchReasonText.setText(getString(R.string.switchReason_format, scoreSheet.getSwitchReasonAt(turn)));
 
         turnText.setBackground( background);
+        switchReasonText.setBackground( background);
         player1IncrementText.setBackground( background);
         player1TotalText.setBackground( background);
         player2IncrementText.setBackground(background);
         player2TotalText.setBackground( background);
         ballsOnTableText.setBackground( background);
-        switchReasonText.setBackground( background);
 
         turnText.setGravity(Gravity.CENTER);
+        switchReasonText.setGravity(Gravity.CENTER);
         player1IncrementText.setGravity(Gravity.CENTER);
         player1TotalText.setGravity(Gravity.CENTER);
         player2IncrementText.setGravity(Gravity.CENTER);
         player2TotalText.setGravity(Gravity.CENTER);
         ballsOnTableText.setGravity(Gravity.CENTER);
-        switchReasonText.setGravity(Gravity.CENTER);
 
         turnText.setPadding(4, 4, 4, 4);
+        switchReasonText.setPadding(4, 4, 4, 4);
         player1IncrementText.setPadding(4, 4, 4, 4);
         player1TotalText.setPadding(4, 4, 4, 4);
         player2IncrementText.setPadding(4, 4, 4, 4);
         player2TotalText.setPadding(4, 4, 4, 4);
         ballsOnTableText.setPadding(4, 4, 4, 4);
-        switchReasonText.setPadding(4, 4, 4, 4);
 
         turnText.setLayoutParams(new TableRow.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT, (float) getResources().getInteger(R.integer.turnnumber_column_weight)));
+        switchReasonText.setLayoutParams(new TableRow.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT, (float) getResources().getInteger(R.integer.switchReason_column_weight)));
         player1IncrementText.setLayoutParams(new TableRow.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT, (float) getResources().getInteger(R.integer.player1Increment_column_weight)));
         player1TotalText.setLayoutParams(new TableRow.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT, (float) getResources().getInteger(R.integer.player1Total_column_weight)));
         player2IncrementText.setLayoutParams(new TableRow.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT, (float) getResources().getInteger(R.integer.player2Increment_column_weight)));
         player2TotalText.setLayoutParams(new TableRow.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT, (float) getResources().getInteger(R.integer.player2Total_column_weight)));
         ballsOnTableText.setLayoutParams(new TableRow.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT, (float) getResources().getInteger(R.integer.ballsOnTable_subcolumn_weight)));
-        switchReasonText.setLayoutParams(new TableRow.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT, (float) getResources().getInteger(R.integer.switchReason_column_weight)));
 
         newTableRow.addView(turnText);
+        newTableRow.addView(switchReasonText);
         newTableRow.addView(player1IncrementText);
         newTableRow.addView(player1TotalText);
         newTableRow.addView(player2IncrementText);
         newTableRow.addView(player2TotalText);
         newTableRow.addView(ballsOnTableText);
-        newTableRow.addView(switchReasonText);
+
 
         tableLayout.addView(newTableRow);
     }
