c759934c91aff6040987b7e001e5195828da9f16
david <david@aderbauer.org>
2024-07-01 18:30:25 +0200

    made player parcelable

--- app/src/main/java/org/sbv/pockettracker/Player.java
+++ app/src/main/java/org/sbv/pockettracker/Player.java
@@ -1,21 +1,17 @@
 package org.sbv.pockettracker;
 
-public class Player {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
+import org.jetbrains.annotations.Contract;
+
+public class Player implements Parcelable {
     static int winningPoints = 40; //default value
     private String name = "";
     private String club = "";
     private int score;
-
-    public Player(String name, String club) {
-        this.name = name;
-        this.club = club;
-        this.score = 0;
-    }
-
-    public Player(String name) {
-        this.name = name;
-        this.score = 0;
-    }
 
     public Player() {
         this.score = 0;
@@ -37,9 +33,10 @@ public class Player {
         return club;
     }
 
-    public boolean isWinner(){
+    public boolean isWinner() {
         return score >= winningPoints;
     }
+
     public void setName(String name) {
         this.name = name;
     }
@@ -52,7 +49,7 @@ public class Player {
         this.score = score;
     }
 
-    public void swapNameAndClubWith(Player otherPlayer){
+    public void swapNameAndClubWith(Player otherPlayer) {
         String nameBackup = otherPlayer.getName();
         String clubBackup = otherPlayer.getClub();
 
@@ -65,4 +62,38 @@ public class Player {
         System.out.println(this.name + this.club + otherPlayer.getName() + otherPlayer.getClub());
     }
 
+    //parcelable
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel dest, int flags) {
+        dest.writeString(this.name);
+        dest.writeString(this.club);
+        dest.writeInt(this.score);
+    }
+
+    public Player(Parcel in) {
+        this.name = in.readString();
+        this.club = in.readString();
+        this.score = in.readInt();
+    }
+
+    public static final Creator<Player> CREATOR = new Creator<Player>() {
+        @NonNull
+        @Contract("_ -> new")
+        @Override
+        public Player createFromParcel(Parcel in) {
+            return new Player(in);
+        }
+
+        @NonNull
+        @Contract(value = "_ -> new", pure = true)
+        @Override
+        public Player[] newArray(int size) {
+            return new Player[size];
+        }
+    };
 }
