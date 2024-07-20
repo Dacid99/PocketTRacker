1a4a904ebdf5fb3dcd92888dbbef04dc517e41c9
SBV <david@aderbauer.org & philipp@aderbauer.org>
2024-07-20 18:43:04 +0200

    added about fragment

---
+++ app/src/main/java/org/sbv/pockettracker/AboutFragment.java
@@ -0,0 +1,26 @@
+package org.sbv.pockettracker;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import java.util.Objects;
+
+public class AboutFragment extends DialogFragment {
+    private View view;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_about, container,false);
+        Objects.requireNonNull(Objects.requireNonNull(getDialog()).getWindow()).setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        return view;
+    }
+}
